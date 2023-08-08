92f867e536b1733e2d48cbc0ccea9b9d044fe141
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-08-09 03:03:05 +0800

    ProjectTager is done with storegit add .

--- src/components/ProjectTager.vue
+++ src/components/ProjectTager.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="projectTager">
         <div class="projectTager__buttons">
-            <button class="projectTager__button" v-for="button in buttonNames" :key="button.id" @click="setChapter(button)">{{ button }}</button>
+            <button class="projectTager__button" v-for="button in this.getProjectPageButtons()" :key="button.id" @click="setChapter(button)">{{ button }}</button>
         </div>
         <div class="projectTager__gallery">
-            <ProjectCard v-for="card in this.currentChapter.cards" :key="card.id" :card="card" :imagePath="currentChapter.chapter"/>
+            <ProjectCard v-for="card in this.getSelectedCard.cards" :key="card.id" :card="card" :imagePath="getSelectedCard.chapter"/>
         </div>
         <Pagination />
     </div>
@@ -12,343 +12,36 @@
 <script>
 import ProjectCard from './ProjectCard.vue';
 import Pagination from './Pagination.vue';
-// import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 export default {
     name: 'ProjectTager',
     components: {
         ProjectCard,
         Pagination
     },
-    data() {
-        return {
-            buttonNames: ['Bathroom', 'Bedroom', 'Kitchen', 'Living Area'],
-            cardsData: [
-                {
-                chapter: 'bedroom',
-                cards: [
-                {
-                    id: 'card1',
-                    span: 3,
-                    image: 'card1.png',
-                    title: 'Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card2',
-                    span: 2,
-                    image: 'card2.png',
-                    title: 'Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card3',
-                    span: 2,
-                    image: 'card3.png',
-                    title: 'Classic Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card4',
-                    span: 3,
-                    image: 'card4.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card5',
-                    span: 3,
-                    image: 'card5.png',
-                    title: 'Minimal Bedroom table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card6',
-                    span: 2,
-                    image: 'card6.png',
-                    title: 'System Table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card7',
-                    span: 2,
-                    image: 'card7.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card8',
-                    span: 2,
-                    image: 'card8.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                }
-                    ]
-                },
-                {
-                chapter: 'bathroom',
-                cards: [
-                {
-                    id: 'card1',
-                    span: 3,
-                    image: 'card1.jpeg',
-                    title: 'Minimal Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card2',
-                    span: 2,
-                    image: 'card2.jpeg',
-                    title: 'Minimal Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card3',
-                    span: 2,
-                    image: 'card3.jpeg',
-                    title: 'Classic Minimal Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card4',
-                    span: 3,
-                    image: 'card4.webp',
-                    title: 'Modern Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card5',
-                    span: 3,
-                    image: 'card5.jpeg',
-                    title: 'Minimal Bathroom table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card6',
-                    span: 2,
-                    image: 'card6.jpeg',
-                    title: 'System Table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card7',
-                    span: 2,
-                    image: 'card7.jpeg',
-                    title: 'Modern Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card8',
-                    span: 2,
-                    image: 'card8.webp',
-                    title: 'Modern Bathroom',
-                    subTitle: 'Decor / Artchitecture'
-                }
-                    ]
-                },
-                {
-                chapter: 'kitchen',
-                cards: [
-                {
-                    id: 'card1',
-                    span: 3,
-                    image: 'card1.jpeg',
-                    title: 'Minimal Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card2',
-                    span: 2,
-                    image: 'card2.jpeg',
-                    title: 'Minimal Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card3',
-                    span: 2,
-                    image: 'card3.jpeg',
-                    title: 'Classic Minimal Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card4',
-                    span: 3,
-                    image: 'card4.jpeg',
-                    title: 'Modern Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card5',
-                    span: 3,
-                    image: 'card5.jpeg',
-                    title: 'Minimal Kitchen table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card6',
-                    span: 2,
-                    image: 'card6.jpeg',
-                    title: 'System Table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card7',
-                    span: 2,
-                    image: 'card7.jpeg',
-                    title: 'Modern Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card8',
-                    span: 2,
-                    image: 'card8.jpeg',
-                    title: 'Modern Kitchen',
-                    subTitle: 'Decor / Artchitecture'
-                }
-                    ]
-                },
-                {
-                chapter: 'livingArea',
-                cards: [
-                {
-                    id: 'card1',
-                    span: 3,
-                    image: 'card1.jpeg',
-                    title: 'Minimal Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card2',
-                    span: 2,
-                    image: 'card2.jpeg',
-                    title: 'Minimal Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card3',
-                    span: 2,
-                    image: 'card3.jpeg',
-                    title: 'Classic Minimal Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card4',
-                    span: 3,
-                    image: 'card4.jpeg',
-                    title: 'Modern Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card5',
-                    span: 3,
-                    image: 'card5.jpeg',
-                    title: 'Minimal Living Area table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card6',
-                    span: 2,
-                    image: 'card6.webp',
-                    title: 'System Table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card7',
-                    span: 2,
-                    image: 'card7.jpeg',
-                    title: 'Modern Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card8',
-                    span: 2,
-                    image: 'card8.png',
-                    title: 'Modern Living Area',
-                    subTitle: 'Decor / Artchitecture'
-                }
-                    ]
-                }
-            ],
-            currentChapter: {
-                chapter: 'bedroom',
-                cards: [
-                {
-                    id: 'card1',
-                    span: 3,
-                    image: 'card1.png',
-                    title: 'Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card2',
-                    span: 2,
-                    image: 'card2.png',
-                    title: 'Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card3',
-                    span: 2,
-                    image: 'card3.png',
-                    title: 'Classic Minimal Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card4',
-                    span: 3,
-                    image: 'card4.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card5',
-                    span: 3,
-                    image: 'card5.png',
-                    title: 'Minimal Bedroom table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card6',
-                    span: 2,
-                    image: 'card6.png',
-                    title: 'System Table',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card7',
-                    span: 2,
-                    image: 'card7.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                },
-                {
-                    id: 'card8',
-                    span: 2,
-                    image: 'card8.png',
-                    title: 'Modern Bedroom',
-                    subTitle: 'Decor / Artchitecture'
-                }
-                ]
+    methods: {
+        ...mapMutations(['changeSelectedChapter']),
+        ...mapGetters(['getCardsData', 'getProjectPageButtons']),
+        setChapter(buttonName) {
+            this.changeSelectedChapter(buttonName);
+            switch (buttonName) {
+                case 'Bedroom':
+                    this.currentChapter = this.$store.getters.getCardsData.filter((item) => item.chapter === 'bedroom').at(0);
+                    break;
+                case 'Bathroom':
+                    this.currentChapter = this.$store.getters.getCardsData.filter((item) => item.chapter === 'bathroom').at(0);
+                    break;
+                case 'Kitchen':
+                    this.currentChapter = this.$store.getters.getCardsData.filter((item) => item.chapter === 'kitchen').at(0);
+                    break;
+                case 'Living Area':
+                this.currentChapter = this.$store.getters.getCardsData.filter((item) => item.chapter === 'livingArea').at(0);
+                    break;
             }
         }
     },
-    methods: {
-        // ...mapGetters(['getCardsData']),
-        setChapter(buttonName) {
-            switch (buttonName) {
-                case 'Bedroom':
-                    this.currentChapter = this.cardsData.at(0);
-                    break;
-                case 'Bathroom':
-                    this.currentChapter = this.cardsData.at(1);
-                    break;
-                case 'Kitchen':
-                    this.currentChapter = this.cardsData.at(2);
-                    break;
-                case 'Living Area':
-                    this.currentChapter = this.cardsData.at(3);
-                    break;
-            }
-        }
+    computed: {
+        ...mapGetters(['getSelectedCard'])
     }
 }
 </script>
