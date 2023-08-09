23d1fbb43f138509e131033373cfe33ee3b56c23
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-08-10 03:31:57 +0800

    Changed all pictures in webp type. And put data for main page in store

--- src/components/landing/MainBlog.vue
+++ src/components/landing/MainBlog.vue
@@ -5,40 +5,20 @@
             <p class="blog__headerDescription">It is a long established fact that a reader will be distracted by the of readable content of a page when lookings at its layouts the points of using.</p>
         </header>
         <div class="blog__designs">
-            <BlogCard v-for="card in cards" :key="card.id" :card="card"/>
+            <BlogCard v-for="card in getDesignData.slice(0,3)" :key="card.id" :card="card"/>
         </div>
     </article>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 import BlogCard from './BlogCard.vue';
 export default {
     name: 'MainBlog',
     components: {
     BlogCard
-},
-    data() {
-        return {
-            cards: [
-                {
-                    image: 'Design1.png',
-                    labelText: 'Kitchen Design',
-                    title: 'Let’s Get Solution For Building Construction Work',
-                    date: '26 December, 2022'
-                },
-                {
-                    image: 'Design2.png',
-                    labelText: 'Living Design',
-                    title: 'Low Cost Latest Invented Interior Designing Ideas',
-                    date: '22 September, 2022'
-                },
-                {
-                    image: 'Design3.png',
-                    labelText: 'Interior Design',
-                    title: 'Best For Any Office & Business Interior Solution',
-                    date: '25 December, 2022'
-                }
-            ]
-        }
+    },
+    computed: {
+        ...mapGetters(['getDesignData'])
     }
 }
 </script>
