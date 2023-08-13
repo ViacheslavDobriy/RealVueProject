4809c277dd6dca541f70de05200bef0562065cb6
ViacheslavDobriy <dobrov.slava@yandex.ru>
2023-08-14 02:27:28 +0800

    Fixed some breadcrumbs, and hovers effects on projectTager page

--- src/components/SubHeader.vue
+++ src/components/SubHeader.vue
@@ -2,7 +2,7 @@
     <div class="poster" :id="posterData.id">
         <div class="poster__intro" v-show="posterData.isTitleExist">
             <h1 class="poster__title">{{ posterData.title }}</h1>
-            <p class="poster__text">{{ posterData.breadcrumbs }}</p>
+            <p><a href="#" class="poster__breadcrumb" v-for="breadcrumb in posterData.breadcrumbs" :key="breadcrumb.id">{{ breadcrumb }}</a></p>
         </div>
     </div>
 </template>
